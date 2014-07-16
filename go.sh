55cfe466763525fe7c6fb581742b40c6a3e8b869
Administrator <admin@162.243.140.145>
2014-07-16 18:39:45 +0100

    Added .sh

---
+++ go.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo “The ultimate install script for jack audio and QT”
+sudo add-apt-repository ppa:kxstudio-team/ppa
+sudo apt-get update 
+sudo apt-get upgrade 
+sudo apt-get install git jack2 libjack-jackd2-dev jack-tools ant jackd qjackctl openjdk-6-jdk fftw3 qjackctl qt-sdk qt-creator screen
+sudo chmod o+rw /usr/include
+#sudo gedit /etc/security/limits.d/audio.conf
+sudo echo “@audio - rtprio 95” >> /etc/security/limits.d/audio.conf
+sudo echo “@audio - memlock unlimited” >> /etc/security/limits.d/audio.conf
+sudo adduser $USER audio
